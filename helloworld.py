24747144851f27df9673b1731b74c324dbe8701c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-24 23:40:10 +0300

    A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:

--- helloworld.py
+++ helloworld.py
@@ -18,7 +18,7 @@ if 5 > 2:
 # Python variables
 
 x = 5
-y = "Hello World!"
+y = "Hello Kelvin!"
 
 print(x)
 print(y)
@@ -26,4 +26,9 @@ print(y)
 # Comments start with a #, and Python will render the rest of the line as a comment:
 
 # This is a comment.
-print("Hello, World!")
+print("Hello, Kipkemboi!")
+
+# A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+
+#print("Hello, World!")
+print("Cheers, Mate!")
