3c9e9828e618db1da4707f0b64034b2c820066fc
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-25 00:00:37 +0300

    Variable names

--- variables.py
+++ variables.py
@@ -47,3 +47,21 @@ A = "Ess K"
 
 print(a)
 print(A)
+
+
+# Variable names
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
