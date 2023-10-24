1028660d60a7cd02f164b5c36d3bbb7aff04f376
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-24 23:27:14 +0300

    You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:

--- helloworld.py
+++ helloworld.py
@@ -9,3 +9,8 @@ if 5 > 2:
     print("Five is greater than two.")
 if 5 < 8:
     print("Five is less than six.")
+
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two!")  
