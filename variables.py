b4d42723b8859ef7734131d66dcd3a8007cfe3ae
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-24 23:53:18 +0300

    Casting

--- variables.py
+++ variables.py
@@ -9,3 +9,21 @@ age = 27
 
 print(name)
 print(age)
+
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+x = 4
+x = "Ess K"
+print(x)
+
+
+# Casting
+# If you want to specify the data type of a variable, this can be done with casting.
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
