0d4dcfad38881988b2142c4f6894aa5de6d4b8a2
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-24 23:48:53 +0300

    Python variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Creating Variables
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+
+name = "Mutai Kelvin"
+age = 27
+
+print(name)
+print(age)
