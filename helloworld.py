8f1190fc679d892d72cb4d0230f2be630b73225c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-24 23:18:31 +0300

    Python syntax Python uses indentation to indicate a block of code.

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 print("Hello, World!")
+
+# Python Syntax
+# Python Indentation
+# Python uses indentation to indicate a block of code.
+
+if 5 > 2:
+    print("Five is greater than two.")
+if 5 < 8:
+    print("Five is less than six.")
