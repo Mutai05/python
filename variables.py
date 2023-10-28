06fa0c6edebb4b48d040a3ac0d4db9a55f915bee
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-29 00:07:35 +0300

    If you use the global keyword, the variable belongs to the global scope:

--- variables.py
+++ variables.py
@@ -137,3 +137,15 @@ def myfunc():
 myfunc()
 
 print("Python is " + x)
+
+
+
+# If you use the global keyword, the variable belongs to the global scope:
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
