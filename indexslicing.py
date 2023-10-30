65799c7d2c1b685b9078ac5f27dfaf8966abaaab
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-30 23:31:38 +0300

    Indexing and slicing in Python

---
+++ indexslicing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+text = "Hello, World!"
+first_char = text[0]  # Access the first character 'H'
+
+
+substring = text[0:5]  # Extracts "Hello"
+
+print(first_char)
+print(substring)
