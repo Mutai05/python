f59cb9b63afdc23709878be4f28b1d33a52ecc4b
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-31 23:22:32 +0300

    Python quiz

---
+++ quiz.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+# Quiz 0
+# 0. What do these lines print?
+
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
+
+# Question #1
+# 1. What do these lines print?
+
+for i in range(2, 4):
+    print(i, end=" ")
+
+# Question #2
+# 2. What do these lines print?
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+
+# Question #3
+# What do these lines print?
+
+for i in range(2, 10, 2):
+    print(i, end=" ")
+
+
+# Question #4
+# What do these lines print?
+
+if 12 == 48/4:
+    print("Holberton")
+else:
+    print("School")
+
+# Question #5
+# What do these lines print?
+
+if True:
+    print("Holberton")
+else:
+    print("School")
+
+# Question #6
+# What do these lines print?
+
+if 12 == 48/3 or 12 is 12:
+    print("Holberton")
+else:
+    print("School")
+
+# Question #7
+# What do these lines print?
+
+a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Holberton")
+    else:
+        print("C is fun")
+else:
+    print("School")
+
+# Question #8
+# What do these lines print?
+
+for i in range(4):
+    print(i, end=" ")
