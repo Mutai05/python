9d6419ba1d35e61568f3a480a005c12c98593e5b
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-02 22:37:12 +0300

    Python Quiz two

---
+++ quiz2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+# Question #0
+# What do these lines print?
+
+def my_function(counter=89):
+     return counter + 1
+
+print(my_function())
+
+# Question #1
+# What do these lines print?
+
+def my_function(counter):
+     print("Counter: {}".format(counter))
+ 
+my_function(12)
+
+# Question #2
+# What do these lines print?
+
+def my_function():
+    print("In my function")
+ 
+my_function
+
+# Question #3
+# What do these lines print?
+
+def my_function():
+    print("In my function")
+ 
+my_function()
+
+# Question #4
+# What do these lines print?
+
+def my_function(counter=89):
+     print("Counter: {}".format(counter))
+
+my_function(12)
+
+# Question #5
+# What do these lines print?
+
+def my_function(counter=89):
+     print("Counter: {}".format(counter))
+ 
+my_function()
