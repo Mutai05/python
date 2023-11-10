e2d4b975300a3fd88cc8898980c1c3eebccc6a80
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-10 23:57:36 +0300

    Data  type float

--- datatypes.py
+++ datatypes.py
@@ -7,9 +7,16 @@ x = 5
 print(type(x)) 
 
 a = "Hello World"
-
 #display x:
 print(a)
 
 #display the data type of x:
 print(type(a)) 
+
+b = 20.5
+
+#display x:
+print(b)
+
+#display the data type of x:
+print(type(b)) 
